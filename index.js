aaef0fcc79b2a586861bd032c755f74f2a3ac4b1
Jett <jettc505@gmail.com>
2022-11-16 15:11:30 -0700

    added the basiccontent that is riten to the file

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// TODO: Include packages needed for this application
+//  Include packages needed for this application
 const inquirer = require("inquirer")
 const fs = require("fs")
-// TODO: Create an array of questions for user input
+
+// file that will be riten to
+const file  = "./README.md"
+
+// questions for user input
 const questions = [
     {
         type:"input",
         message:"Enter your Email address",
-        name:"Email",
+        name:"email",
     },
     {
         type:"input",
@@ -37,60 +41,65 @@ const questions = [
         type:"list",
         message:"Enter the licencing",
         choices:["None","Apache License 2.0", "GNU General Public License v3.0", 'MIT License', 'BSD 2-Clause "Simplified" Licensev', 'BSD 3-Clause "New" or "Revised" License', "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"],
-        name:"licenc",
+        name:"license",
     },
     {
         type:"input",
         message:"Enter Contributing criteria",
-        name:"contribution",
+        name:"contributing",
     }, 
     {
         type:"input",
         message:"Enter Tests",
         name:"test",
     }, 
-    {
-        type:"input",
-        message:"Enter questions",
-        name:"questions",
-    },
-    {
-        type:"input",
-        message:"How can one reach you for additional questions",
-        name:"addQuestions",
-    },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    `# ${"Title"}
 
-    ## Description
-
-    ### Table of Contents
-
-    ### Installing
-
-    ### Usage
-
-    ### License
-
-    ### Contributig
-
-    ### Tests
-
-    ### Questions` 
-
-}
-
-// TODO: Create a function to initialize app
+// initialize app
 function init() {
     inquirer
         .prompt(questions)
-        .then((data) =>
-            console.log("Jett")
-    );
+        .then(({email, username, title, description, installation, usage, license, contributing, test, }) => {
+            const renderREADME =`
+# ${title}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+## Instalation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## License
+
+${license}
+
+## Contributing
+
+${contributing}
+
+### Test
+                
+${test}
+
+### Questions
+
+${username}
+
+${email}`
+
+            fs.writeFile(file, renderREADME, (err) =>
+                err ? console.error(err) : console.log("File Generated")
+            );
+        });
 }
 
 init()
-
